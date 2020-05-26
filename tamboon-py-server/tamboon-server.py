8a184a5d5a8cc0c6952b1e72fda8eb0c3612bb40
Md. Elme Focruzzaman Razi <shuvo@lighsntunes.com>
2020-05-27 02:32:36 +0600

    Added commandline arument features
    Signed-off-by: Md. Elme Focruzzaman Razi <shuvo@lighsntunes.com>

--- tamboon-py-server/tamboon-server.py
+++ tamboon-py-server/tamboon-server.py
@@ -1,14 +1,57 @@
+import getopt
+import sys
+import socket
+
 from flask import Flask
 from flask_restful import Api
 
 from charities import Charities
 from donations import Donations
 
-app = Flask(__name__)
-api = Api(app)
+secret_key = ''
+public_key = ''
+host_ip = '127.0.0.1'
+host_port = '8088'
 
-api.add_resource(Charities, '/charities')
-api.add_resource(Donations, '/donations')
+
+def get_host():
+     hostname = socket.gethostname()
+     ## getting the IP address using socket.gethostbyname() method
+     ip_address = socket.gethostbyname(hostname)
+     ## printing the hostname and ip_address
+     # print "Hostname: " + hostname
+     # print "IP Address: " + ip_address
+     return ip_address
+
+def parse_keys(argv):
+     global secret_key, public_key, host_port
+     try:
+          opts, args = getopt.getopt(argv, "hs:p:", ["skey=", "pkey=","port="])
+     except getopt.GetoptError:
+          print 'tamboon-server.py -s <secret key> -p <public key>'
+          sys.exit(2)
+     for opt, arg in opts:
+          if opt == '-h':
+               print 'tamboon-server.py -s <secret key> -p <public key>'
+               sys.exit()
+          elif opt in ("-s", "--skey"):
+               secret_key = arg
+          elif opt in ("-p", "--pkey"):
+               public_key = arg
+          elif opt in ("--port"):
+               host_port = arg
+
+def start_server():
+     host_ip = get_host()
+     app = Flask(__name__)
+     api = Api(app)
+     api.add_resource(Charities, '/charities')
+     api.add_resource(Donations, '/donations')
+     app.run(host = host_ip, port = host_port)
+
+def main(argv):
+     parse_keys(argv)
+     start_server()
 
 if __name__ == '__main__':
-     app.run(port='5002')
+     main(sys.argv[1:])
