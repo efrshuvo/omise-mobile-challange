17bb1952e7cfa980d148f101ed3def21c355d42a
Md. Elme Focruzzaman Razi <shuvo@lighsntunes.com>
2020-05-28 15:02:29 +0600

    Add Secrete Key and Public Key Warning Message

--- tamboon-py-server/tamboon-server.py
+++ tamboon-py-server/tamboon-server.py
@@ -10,11 +10,27 @@ from chargeoperation import ChargeOperation
 secret_key = os.getenv("OMISE_SKEY")
 public_key = os.getenv("OMISE_PKEY")
 
-print 'Secrete Key: '+secret_key
-print 'Public Key: '+secret_key
+if secret_key != None:
+     print 'Secrete Key: '+secret_key
+else:
+     print 'Secret key is required.'
+     print 'Without Secrete key /donation api is not work properly.'
+     print 'Please stop server using Ctrl+C command.'
+     print 'You can set Secrete key using following command.'
+     print '\t\texport OMISE_SKEY=<Your Secrete Key>'
+
+
+if public_key != None:
+     print 'Public Key: '+public_key
+else:
+     print 'Public key is optional.'
+     print 'You can set Public key using following command.'
+     print '\t\texport OMISE_PKEY=<Your Public Key>'
 
 ChargeOperation.Instance().set_secret_key(secret_key)
+
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(Charities, '/charities')
 api.add_resource(Donations, '/donations')
+
