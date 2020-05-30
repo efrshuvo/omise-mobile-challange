b5cc134b1dec87b4eaf1941f546fd83dc692a0f3
Md. Elme Focruzzaman Razi <shuvo@lighsntunes.com>
2020-05-30 13:53:33 +0600

    Adding donation , settings and char ity list view and added a view packages

---
+++ tamboon-android-client/app/src/main/java/com/example/tamboon/views/CharityListActivity.java
@@ -0,0 +1,16 @@
+package com.example.tamboon.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tamboon.R;
+
+public class CharityListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- tamboon-android-client/app/src/main/java/com/example/tamboon/views/DonationActivity.java
+++ tamboon-android-client/app/src/main/java/com/example/tamboon/views/DonationActivity.java
@@ -1,14 +1,16 @@
-package com.example.tamboon;
+package com.example.tamboon.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.tamboon.R;
+
+public class DonationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_donation);
     }
 }
