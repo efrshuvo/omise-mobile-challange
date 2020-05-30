6ae2c3e677b184c04ac46dc6eb51e59d5fd6b187
Md. Elme Focruzzaman Razi <shuvo@lighsntunes.com>
2020-05-30 21:49:42 +0600

    Implemented model classes

--- tamboon-android-client/app/src/main/java/com/devlab/tamboon/models/Charities.java
+++ tamboon-android-client/app/src/main/java/com/devlab/tamboon/models/Charities.java
@@ -1,4 +1,31 @@
 package com.devlab.tamboon.models;
 
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Charities {
+
+    @SerializedName("data")
+    @Expose
+    private List<Charity> data = null;
+    @SerializedName("total")
+    @Expose
+    private Integer total;
+
+    public List<Charity> getData() {
+        return data;
+    }
+
+    public void setData(List<Charity> data) {
+        this.data = data;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
 }
--- tamboon-android-client/app/src/main/java/com/devlab/tamboon/models/Donation.java
+++ tamboon-android-client/app/src/main/java/com/devlab/tamboon/models/Donation.java
@@ -1,4 +1,32 @@
 package com.devlab.tamboon.models;
 
 public class Donation {
+
+    private String name;
+    private String token;
+    private Integer amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
 }
