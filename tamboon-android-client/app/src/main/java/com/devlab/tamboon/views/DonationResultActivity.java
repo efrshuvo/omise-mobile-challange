81c4abc75529116a88ef94ad24d0c8642fab7cf2
Md. Elme Focruzzaman Razi <shuvo@lighsntunes.com>
2020-05-31 16:22:00 +0600

    Rename some files with more meaningful name

--- tamboon-android-client/app/src/main/java/com/devlab/tamboon/views/DonationResultActivity.java
+++ tamboon-android-client/app/src/main/java/com/devlab/tamboon/views/DonationResultActivity.java
@@ -11,6 +11,6 @@ public class DonationResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_donation_result);
     }
 }
